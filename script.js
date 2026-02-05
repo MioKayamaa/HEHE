768a42bf8255379108bd4830f78a9926ba8e8769
MioKayamaa <kus-berna@hotmail.com>
2026-02-05 19:00:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -71,19 +71,19 @@ let messageIndex = 0;
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
-    const gif = document.getElementById('valentineGif'); // on récupère le GIF
+    const gif = document.getElementById('main-gif');
 
-    
+  
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
 
-    
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+    yesButton.style.fontSize = `${currentSize * 1.3}px`;
 
     
-    gif.src = "cutegif/gif2.gif";
+    gif.src = "gif2.gif";
 }
+
 
 function handleYesClick() {
     window.location.href = "yes_page.html";
